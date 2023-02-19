584a8b3a9403175bbdcf78381992cbea8822c9a2
Shaan-kapoor <100303945+Shaan-kapoor@users.noreply.github.com>
2023-02-19 19:22:11 +0530

    Added new fuction to get video IDs(not working Rn)

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-var videoIDs = [
-  "C0DPdy98e4c",
-  "LXb3EKWsInQ",
-  "mkggXE5e2yk",
-  // Add more video IDs here
-];
+// Define your API key and playlist IDs
+// const API_KEY = "AIzaSyDzbugo29YxvzUpGhe7pPYABlGLH9NPaUM";
+// const playlistIds = ["PLyORnIW1xT6wqvszJbCdLdSjylYMf3sNZ", "PL9bw4S5ePsEG1Ovrxj9o2RLx43ALFoFyU", "PLq-e2Ss4lhca4e40LiLrkjD-2EUQ7sEf8"];
+// let videoIds = [];
+// // Function to retrieve video IDs from multiple playlists
+// async function getVideoIdsFromPlaylists(playlistIds) {
+//   try {
+//     let videoIds = [];
+
+//     // Iterate over each playlist ID and retrieve its video IDs
+//     for (const playlistId of playlistIds) {
+//       let nextPageToken = "";
+//       let videosLeft = true;
+
+//       while (videosLeft) {
+//         const response = await fetch(
+//           `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken=${nextPageToken}&playlistId=${playlistId}&key=${API_KEY}`
+//         );
+//         const data = await response.json();
+//         const playlistVideoIds = data.items.map(
+//           (item) => item.snippet.resourceId.videoId
+//         );
+//         videoIds = [...videoIds, ...playlistVideoIds];
+//         nextPageToken = data.nextPageToken;
+//         videosLeft = nextPageToken !== undefined;
+//       }
+//     }
+
+//     // Return the combined video IDs as an array
+//     return videoIds;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+let videoIds = ["ODY6JWzS8WU"];
 
 function playRandomSong() {
-  var randomIndex = Math.floor(Math.random() * videoIDs.length);
-  var videoID = videoIDs[randomIndex];
+  var randomIndex = Math.floor(Math.random() * videoIds.length);
+  var videoId = videoIds[randomIndex];
   var player = document.getElementById("player");
-  player.src = "https://www.youtube.com/embed/" + videoID + "?autoplay=1";
+  player.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
 }
 
 window.onload = function() {
+  //getVideoIdsFromPlaylists(playlistIds);
   playRandomSong();
+
 };
 
